1a89a70ab78627a07fa6c5d78fb9d120888f864e
FaustoFaggion <faustofaggion@hotmail.com>
2022-12-27 18:13:05 -0300

    performance test show functions wit range have to be upgrated because the performance are too slow due alloc.construct instruction when the type is integral

--- tests/performance.cpp
+++ tests/performance.cpp
@@ -499,7 +499,7 @@ int	main(void) {
 }
 
 
-	std::cout << "ERASE" << std::endl;
+	std::cout << "ERASE RANGE" << std::endl;
 {
 	clock_t	t1_std;
 	clock_t	t2_ft;
@@ -556,5 +556,66 @@ int	main(void) {
 	std::cout << "string: " << (t6_ft / t5_std) << "    std: " << t5_std << " ft: " << t6_ft << std::endl;
 	std::cout << "int:    " << (t8_ft / t7_std) << "    std: " << t7_std << " ft: " << t8_ft << std::endl;
 }
+
+
+{
+	std::cout << "INSERT" << std::endl;
+	
+	clock_t	t1_std;
+	clock_t	t2_ft;
+	clock_t	t3_std;
+	clock_t	t4_ft;
+	clock_t	t5_std;
+	clock_t	t6_ft;
+	clock_t	t7_std;
+	clock_t	t8_ft;
+	int		n = 1000000;
+	
+	ft::vector<std::string> str_ft0(n, "a");
+	std::vector<std::string> str_std0(n, "a");
+	ft::vector<int> int_ft0(n, 2);
+	std::vector<int> int_std0(n, 2);
+
+	ft::vector<std::string> str_ft;
+	std::vector<std::string> str_std;
+	ft::vector<int> int_ft;
+	std::vector<int> int_std;
+	
+	for (int i = 0; i < 10; i++) {
+		t1_std = std::clock();
+		str_std0.insert(str_std0.begin() + 2, "a");
+		t1_std = std::clock() - t1_std;
+		t5_std+= t1_std;
+		str_std0.pop_back();
+	}
+
+	for (int i = 0; i < 10; i++) {
+		t2_ft = std::clock();
+		str_ft0.insert(str_ft0.begin() + 2, "a");
+		t2_ft = std::clock() - t2_ft;
+		t6_ft += t2_ft;
+		str_ft0.pop_back();
+	}
+
+	for (int i = 0; i < 10; i++) {
+		t3_std = std::clock();
+		int_std0.insert(int_std0.begin() + 2, 1);
+		t3_std = std::clock() - t3_std;
+		t7_std += t3_std;
+		int_std0.pop_back();
+	}
+
+	for (int i = 0; i < 10; i++) {
+		t4_ft = std::clock();
+		int_ft0.insert(int_ft0.begin() + 2, 1);;
+		t4_ft = std::clock() - t4_ft;
+		t8_ft += t4_ft;
+		int_ft0.pop_back();
+	}
+
+	std::cout << "string: " << (t6_ft / t5_std) << "    std: " << t5_std << " ft: " << t6_ft << std::endl;
+	std::cout << "int:    " << (t8_ft / t7_std) << "    std: " << t7_std << " ft: " << t8_ft << std::endl;
+}
+
 
 }
